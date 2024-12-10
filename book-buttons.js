ce9d179359d6936b7d548701ef8db73f9738f4f1
MelodyParker <102301694+MelodyParker@users.noreply.github.com>
2024-12-10 13:13:47 -0600

    first-commit

---
+++ book-buttons.js
@@ -0,0 +1,108 @@
+const books = [
+  {
+    title: "Catch-22",
+    imgPath: "/covers/Catch-22.jpg",
+    imgCropping: [
+      {
+        top: 0,
+        left: 1110,
+        bottom: 545,
+        right: 1231
+      },
+      {
+        top: 0,
+        left: 1231,
+        bottom: 766,
+        right: 1509
+      },
+      {
+        top: 0,
+        left: 1509,
+        bottom: 620,
+        right: 1920
+      }
+    ]
+  },
+  {
+    title: "The Importance of Being Earnest",
+    imgPath: "/covers/Earnest.jpg",
+    imgCropping: [
+      {
+        top: 545,
+        left: 850,
+        bottom: 1080,
+        right: 1232
+      }
+    ]
+  },
+  // {
+  //   title: "The Handmaid's Tale",
+  //   imgPath: "/covers/Handmaid's Tale.jpg"
+  // },
+  // {
+  //   title: "The Hitchhiker's Guide to the Galaxy",
+  //   imgPath: "/covers/Hitchhiker's Guide.jpg"
+  // },
+  {
+    title: "Pride and Prejudice",
+    imgPath: "/covers/Pride and Prejudice.jpg",
+    imgCropping: [
+      {
+        top: 621,
+        left: 1510,
+        bottom: 1080,
+        right: 1920
+      }
+    ]
+  },
+  {
+    title: "Slaughterhouse-Five",
+    imgPath: "/covers/Slaughterhouse-Five.jpg",
+    imgCropping: [
+      {
+        top: 0,
+        left: 0,
+        right: 1110,
+        bottom: 543
+      },
+      {
+        top: 543,
+        left: 0,
+        right: 843,
+        bottom: 740
+      }
+    ]
+  }
+];
+
+function bookButtonPressed(e) {
+  let bookButtonId = e.target.parentNode.id;
+  console.log(e.target.parentNode)
+  let actualId = parseInt(bookButtonId.substring(12));
+  console.log(actualId)
+  let bookSelected = books[actualId];
+  for (let img of bookSelected.imgCropping) {
+    makeSectionColored(img.left, img.top, img.right - img.left, img.bottom - img.top);
+  }
+}
+
+
+let bookElementsToAdd = [];
+
+for (let i=0; i<books.length; i++) {
+  let book = books[i];
+  let el = document.createElement("button");
+  let img = document.createElement("img");
+  img.src = book.imgPath;
+  img.width = 100;
+  el.appendChild(img);
+  el.id = "book-button-" + i;
+  el.addEventListener("click", bookButtonPressed);
+  bookElementsToAdd.push(el);
+}
+
+for (let bookEl of bookElementsToAdd) {
+  document.body.appendChild(bookEl)
+}
+
+
---
+++ script.js
@@ -0,0 +1,68 @@
+let bwImg;
+let colorImg;
+let startMouseX, startMouseY;
+
+function preload() {
+  bwImg = loadImage("/Genius Hour.png");
+  colorImg = loadImage("/Genius Hour.png")
+}
+
+
+function setup() {
+  let canvas = createCanvas(1920, 1080)
+  canvas.parent("canvas-container")
+  bwImg.loadPixels();
+  for (let y=0; y<bwImg.height; y++) {
+    for (let x=0; x<bwImg.width; x++) {
+      let index = (x + y * width) * 4;
+      let r = bwImg.pixels[index+0];
+      let g = bwImg.pixels[index+1];
+      let b = bwImg.pixels[index+2];
+      // let a = pixels[index+3];
+      let bw = (r + g + b)/3;
+            
+      bwImg.pixels[index] = bw;
+      bwImg.pixels[index + 1] = bw;
+      bwImg.pixels[index + 2] = bw;
+    }
+  }
+  bwImg.updatePixels();
+}
+
+
+function draw() {
+  background(0)
+  image(bwImg, 0, 0, bwImg.width, bwImg.height);
+}
+
+function mousePressed() {
+  startMouseX = mouseX;
+  startMouseY = mouseY;
+}
+
+function mouseReleased() {
+  makeSectionColored(startMouseX, startMouseY, mouseX - startMouseX, mouseY - startMouseY)
+}
+
+
+
+function makeSectionColored(sectionX, sectionY, sectionWidth, sectionHeight) {
+  console.log(sectionX, sectionY, sectionWidth, sectionHeight)
+  bwImg.loadPixels();
+  colorImg.loadPixels();
+  for (let y=sectionY; y<sectionY + sectionHeight; y++) {
+    for (let x=sectionX; x<sectionX + sectionWidth; x++) {
+      let index = (x + y * width) * 4;
+      let r = colorImg.pixels[index];
+      let g = colorImg.pixels[index + 1];
+      let b = colorImg.pixels[index + 2];
+            
+      bwImg.pixels[index] = r;
+      bwImg.pixels[index + 1] = g;
+      bwImg.pixels[index + 2] = b;
+    }
+    
+  }
+  colorImg.updatePixels();
+  bwImg.updatePixels();
+}
